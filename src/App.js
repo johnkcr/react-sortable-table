e536bc6ef38cd39568a44046874ffe041ee574fe
Ramon Cora <ramon.cora.amaya@gmail.com>
2021-05-05 14:35:25 -0400

    remove console logs

--- src/App.js
+++ src/App.js
@@ -154,7 +154,6 @@ export default function App() {
       .get('https://gist.githubusercontent.com/ryanjn/07512cb1c008a5ec754aea6cbbf4afab/raw/eabb4d324270cf0d3d17a79ffb00ff3cfaf9acc3/orders.json')
       .then(response => {
         const orders = keysToCamel(response.data);
-        console.log(normalizeOrders(orders));
         setOrders(normalizeOrders(orders));
       }).catch(err => {
         console.error(err);
